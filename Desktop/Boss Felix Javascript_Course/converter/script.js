e42ebfa9807bd4260e09ca684076e4bec51f4e35
afingi-kingsley <afingikingsley638@gamil.com>
2025-03-04 12:15:05 +0000

    java practice

---
+++ Desktop/Boss Felix Javascript_Course/converter/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+// I created a function to convert Kilometer  to mile
+
+ const kilometerConvert = prompt('input kilometer number here')
+//  console.log(kilometerConvert);
+ const kilo=kilometerConvert / 1.60934;
+ console.log(`${kilometerConvert} Kilometers is equal to ${kilo} Miles`);
